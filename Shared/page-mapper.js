529d4c961cb07954ce37c405ae8ba2309ef82eb3
SjoerdKingma <Sjoerdk@outlook.com>
2022-05-06 19:24:41 +0200

    Added naming conventions.

--- Shared/page-mapper.js
+++ Shared/page-mapper.js
@@ -1,14 +1,14 @@
 const bookmarks_htmlPages = [
     //ElementId, URL, Menu depth (0: no depth. 1: first sub menu.)
-    ["navButton_about", "introduction.html", 0],
-    ["navButton_about_introduction", "introduction.html", 1],
-    ["navButton_about_contact", "contact.html", 1],
-    ["navButton_projects", "projects.html", 0],
-    ["navButton_projects_search", "projects.html", 1],
-    ["navButton_components", "components.html", 0],
-    ["navButton_components_search", "components.html", 1],
-    ["navButton_challenges", "challenges.html", 0],
-    ["navButton_challenges_search", "challenges.html", 1]
+    ["navButton-about", "introduction.html", 0],
+    ["navButton-about-introduction", "introduction.html", 1],
+    ["navButton-about-contact", "contact.html", 1],
+    ["navButton-projects", "projects.html", 0],
+    ["navButton-projects-search", "projects.html", 1],
+    ["navButton-components", "components.html", 0],
+    ["navButton-components-search", "components.html", 1],
+    ["navButton-challenges", "challenges.html", 0],
+    ["navButton-challenges-search", "challenges.html", 1]
   ]
 
   function getBookmarkIndexByElementId(elementId) {
