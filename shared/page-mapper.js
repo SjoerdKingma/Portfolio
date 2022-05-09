d557b472c2d66106d5ae56464f2a07fc67950634
SjoerdKingma <Sjoerdk@outlook.com>
2022-05-09 17:56:54 +0200

    Added support for detail pages.

--- shared/page-mapper.js
+++ shared/page-mapper.js
@@ -5,10 +5,20 @@ const bookmarks_htmlPages = [
     ["navButton-about-contact", "contact.html", 1],
     ["navButton-projects", "projects.html", 0],
     ["navButton-projects-search", "projects.html", 1],
+    ["navButton-projects-current", "", 1],
     ["navButton-components", "components.html", 0],
     ["navButton-components-search", "components.html", 1],
     ["navButton-challenges", "challenges.html", 0],
-    ["navButton-challenges-search", "challenges.html", 1]
+    ["navButton-challenges-search", "challenges.html", 1],
+  ]
+
+  const details_pages = [
+    //Project name, Details URL
+    ["My Portfolio Website", "pages/details/my-portfolio-website.html"],
+    ["Online multiplayer card-game", "pages/details/online-mp-card-game.html"],
+    ["Recycle bin cleaner", "pages/details/recycle-bin-cleaner.html"],
+    ["PHP school assignment", "pages/details/php-school-assignment.html"],
+    ["Easy Calculator", "pages/details/easy-calculator.html"],
   ]
 
   function getBookmarkIndexByElementId(elementId) {
@@ -22,4 +32,13 @@ const bookmarks_htmlPages = [
 
   function getUrlByIndex(index){
     return bookmarks_htmlPages[index][1];
+  }
+
+  function getUrlFromDetailName(detailName){
+    for(var i=0; i<details_pages.length; i++){
+      var currentDetailsPage = details_pages[i];
+      if(currentDetailsPage[0] == detailName){
+        return currentDetailsPage[1];
+      }
+    }
   }
