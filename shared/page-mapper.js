26f81bc28da5c93f5bad0615e9b9e25558f4fd73
SjoerdKingma <Sjoerdk@outlook.com>
2022-05-16 15:37:51 +0200

    Code improvements in index.html

--- shared/page-mapper.js
+++ shared/page-mapper.js
@@ -1,5 +1,5 @@
 const bookmarks_htmlPages = [
-    //ElementId, URL, Menu depth (0: no depth. 1: first sub menu.)
+    //  ElementId,                URL            Nav menu depth
     ["navButton-about", "pages/introduction.html", 0],
     ["navButton-about-introduction", "pages/introduction.html", 1],
     ["navButton-about-contact", "pages/contact.html", 1],
@@ -10,21 +10,21 @@ const bookmarks_htmlPages = [
     ["navButton-components-search", "pages/components.html", 1],
     ["navButton-challenges", "pages/challenges.html", 0],
     ["navButton-challenges-search", "pages/challenges.html", 1],
-  ]
+  ];
 
-  const details_pages = [
-    //Project name, Details URL
+  const projectDetails_htmlPages = [
+    //Project name                         URL
     ["My Portfolio Website", "pages/details/my-portfolio-website.html"],
     ["Online multiplayer card-game", "pages/details/online-mp-card-game.html"],
     ["Recycle bin cleaner", "pages/details/recycle-bin-cleaner.html"],
     ["PHP school assignment", "pages/details/php-school-assignment.html"],
     ["Easy Calculator", "pages/details/easy-calculator.html"],
-  ]
+  ];
 
   function getBookmarkIndexByElementId(elementId) {
     for (var i = 0; i < bookmarks_htmlPages.length; i++) {
-      var currentItem = bookmarks_htmlPages[i][0];
-      if (currentItem == elementId) {
+      var currentElementId = bookmarks_htmlPages[i][0];
+      if (currentElementId == elementId) {
         return i;
       }
     }
@@ -34,9 +34,9 @@ const bookmarks_htmlPages = [
     return bookmarks_htmlPages[index][1];
   }
 
-  function getUrlFromDetailName(detailName){
-    for(var i=0; i<details_pages.length; i++){
-      var currentDetailsPage = details_pages[i];
+  function getUrlByDetailName(detailName){
+    for(var i=0; i<projectDetails_htmlPages.length; i++){
+      var currentDetailsPage = projectDetails_htmlPages[i];
 
       if(currentDetailsPage[0] == detailName){
         return currentDetailsPage[1];
